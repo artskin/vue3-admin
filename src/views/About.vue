fed4e90d110b3ede501c01e2ae717647a880a4a6
Guliqiang <guliqiang@sensetime.com>
2020-09-08 21:14:55 +0800

    add components test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    2
+    <TrackView></TrackView>
   </div>
 </template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import TrackView from '@/components/TrackView.vue'; // @ is an alias to /src
+console.log(TrackView)
+@Options({
+  components: {
+    TrackView,
+  },
+})
+export default class Home extends Vue {}
+</script>
